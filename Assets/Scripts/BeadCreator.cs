534900e502bb30ce923ca47565ca0f7422fad702
EQwon <bluetwintail@naver.com>
2017-07-04 11:53:46 +0900

    Bead 그림 추가

---
+++ Assets/Scripts/BeadCreator.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeadCreator : MonoBehaviour
+{
+	public GameObject[] Beads;
+	public int row;
+	public int column;
+
+	private void Start()
+	{
+
+	}
+}
