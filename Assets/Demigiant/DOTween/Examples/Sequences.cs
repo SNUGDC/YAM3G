4b693bde842fffcd2b4b33c99f73c29f3e31edf6
SeongChan Lee <foriequal@gmail.com>
2017-08-15 01:52:14 +0900

    DOTween missing DLL re-imported

--- Assets/Demigiant/DOTween/Examples/Sequences.cs
+++ Assets/Demigiant/DOTween/Examples/Sequences.cs
@@ -4,8 +4,7 @@ using DG.Tweening;
 
 public class Sequences : MonoBehaviour
 {
-	public Transform targetA;
-    public Transform targetB;
+	public Transform target;
 
 	void Start()
 	{
@@ -19,31 +18,18 @@ public class Sequences : MonoBehaviour
 		// Also, during all the tween, we will make sure that the target also moves on the X axis.
 
 		// Create new Sequence object
-		Sequence sequenceA = DOTween.Sequence();
-        Sequence sequenceB = DOTween.Sequence();
-        Sequence mySequence = DOTween.Sequence();
+		Sequence mySequence = DOTween.Sequence();
 		// Add a 1 second move tween only on the Y axis
-		sequenceA.Append(targetA.DOMoveY(2, 1));
+		mySequence.Append(target.DOMoveY(2, 1));
 		// Add a 1 second rotation tween, using Join so it will start when the previous one starts
-		sequenceA.Join(targetA.DORotate(new Vector3(0, 135, 0), 1));
+		mySequence.Join(target.DORotate(new Vector3(0, 135, 0), 1));
 		// Add a 1 second scale Y tween, using Append so it will start after the previously added tweens end
-		sequenceA.Insert(sequenceA.Duration(false), targetA.DOScaleY(0.2f, 1));
+		mySequence.Append(target.DOScaleY(0.2f, 1));
 		// Add an X axis relative move tween that will start from the beginning of the Sequence
 		// and last for the whole Sequence duration
-		sequenceA.Insert(0, targetA.DOMoveX(4, sequenceA.Duration()).SetRelative());
-        
-        sequenceB.Insert(sequenceB.Duration(), targetA.DOMoveY(2, 1).SetRelative());
-        // Add a 1 second rotation tween, using Join so it will start when the previous one starts
-        sequenceB.Join(targetA.DORotate(new Vector3(0, 135, 0), 1));
-        // Add a 1 second scale Y tween, using Append so it will start after the previously added tweens end
-        sequenceB.Append(targetA.DOScaleY(0.2f, 1));
-        // Add an X axis relative move tween that will start from the beginning of the Sequence
-        // and last for the whole Sequence duration
-        sequenceB.Insert(0, targetA.DOMoveX(4, sequenceB.Duration()).SetRelative());
+		mySequence.Insert(0, target.DOMoveX(4, mySequence.Duration()).SetRelative());
 
-        // Oh, and let's also make the whole Sequence loop backward and forward 4 times
-        mySequence.Append(sequenceA);
-        mySequence.Append(sequenceB);
-        mySequence.SetLoops(4, LoopType.Yoyo).Play();
+		// Oh, and let's also make the whole Sequence loop backward and forward 4 times
+		mySequence.SetLoops(4, LoopType.Yoyo);
 	}
 }
